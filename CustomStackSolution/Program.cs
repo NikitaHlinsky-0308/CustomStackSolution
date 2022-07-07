13f62ee1b034f2be08237ce24af4baecd17e28ea
NikitaHlinsky-0308 <75807378+NikitaHlinsky-0308@users.noreply.github.com>
2022-07-07 23:56:01 +0300

    Init commit

---
+++ CustomStackSolution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomStackSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            
+            Console.WriteLine(stack.Peek());
+
+            stack.Pop();
+
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
+
