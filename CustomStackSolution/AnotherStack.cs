4c8c6cf73bf52eed2e10ed91c229b37188d49dfe
NikitaHlinsky-0308 <75807378+NikitaHlinsky-0308@users.noreply.github.com>
2022-07-08 01:07:08 +0300

    Added a generic based stack

---
+++ CustomStackSolution/AnotherStack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace CustomStackSolution
+{
+    public class AnotherStack<T>
+    {
+
+        
+
+        private T[] _items;
+
+        public int Count { get; private set; }
+        public int Capacity
+        {
+            get
+            {
+                return _items.Length;
+            }
+        }
+
+        public AnotherStack()
+        {
+            const int defaultCapacity = 4;
+            _items = new T[defaultCapacity];
+        }
+
+        public AnotherStack(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            if (_items.Length == Count)
+            {
+                T[] largerArray = new T[Count * 2];
+                Array.Copy(_items, largerArray, Count);
+
+                _items = largerArray;
+            }
+
+            _items[Count++] = item;
+        }
+
+        public void Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            _items[--Count] = default;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return _items[Count - 1];
+        }
+
+    }
+}
+
--- CustomStackSolution/Program.cs
+++ CustomStackSolution/Program.cs
@@ -11,13 +11,38 @@ namespace CustomStackSolution
             stack.Push(1);
             stack.Push(2);
             stack.Push(3);
+            stack.Push("qwe");
+            // object based stack allow us to
+            // add any value but it will throw
+            // Err on runtime
 
-            
+
+
             Console.WriteLine(stack.Peek());
 
             stack.Pop();
 
             Console.WriteLine(stack.Peek());
+
+            AnotherStack<int> GenericStack = new AnotherStack<int>();
+
+            GenericStack.Push(1);
+            GenericStack.Push(2);
+            GenericStack.Push(3);
+            // GenericStack.Push("qwe");
+
+            // ide don't allow us to push non T
+            // value
+
+            // with generic we're happy
+
+
+            Console.WriteLine(stack.Peek());
+
+            stack.Pop();
+
+            Console.WriteLine(stack.Peek());
+
         }
     }
 }
